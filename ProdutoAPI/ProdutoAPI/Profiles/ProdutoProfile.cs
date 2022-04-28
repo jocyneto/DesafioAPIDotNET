6dd070a89b061d8e9e2a11e9583fadfeed15e231
Jocy <jocygneto@gmail.com>
2022-04-28 17:06:13 -0300

    adicionando restantes dos codigos

---
+++ ProdutoAPI/ProdutoAPI/Profiles/ProdutoProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using ProdutoAPI.Data.Dtos;
+using ProdutoAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProdutoAPI.Profiles
+{
+    public class ProdutoProfile : Profile
+    {
+
+        public ProdutoProfile()
+        {
+            CreateMap<CreateProdutoDto, Produto>();
+            CreateMap<Produto, ReadProdutoDto>();
+            CreateMap<UpdateProdutoDto, Produto>();
+        }
+
+
+    }
+}
